66d3ac48eec54c8454ea517ac98b0f77d9338db5
usuario <usuario@usuario-BD>
2023-04-17 21:09:38 +0200

    update

--- src/Ventanas/ConseguirMisPedidos.java
+++ src/Ventanas/ConseguirMisPedidos.java
@@ -26,11 +26,16 @@ public class ConseguirMisPedidos extends java.awt.Dialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent evt) {
                 closeDialog(evt);
             }
         });
+
+        jLabel1.setText("jLabel1");
+        add(jLabel1, java.awt.BorderLayout.CENTER);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -62,5 +67,6 @@ public class ConseguirMisPedidos extends java.awt.Dialog {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
