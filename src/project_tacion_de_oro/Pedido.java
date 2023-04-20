197405f7bf451fb80525df06cc966a492b3a982e
Angel <Angel@DESKTOP-LKGFTP4>
2023-04-20 19:47:39 +0200

    clase pedido (angel)

--- src/project_tacion_de_oro/Pedido.java
+++ src/project_tacion_de_oro/Pedido.java
@@ -9,5 +9,50 @@ package project_tacion_de_oro;
  * @author usuario
  */
 public class Pedido {
+    private String fecha;
+    private float total;
+
+    public Pedido(String fecha, float total) {
+        this.fecha = fecha;
+        this.total = total;
+    }
+
+    public Pedido() {
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
+    
+    public static void calcularTotal(){
+        
+    }
+    public static void añadirArticulo(Articulo articulo){
+        
+    }
+    public static void asignarRuta(){
+        
+    }
+    public static void calcularFechaSalida(){
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" + "fecha=" + fecha + ", total=" + total + '}';
+    }
+    
     
 }
