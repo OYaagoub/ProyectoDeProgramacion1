01e312627295513ff54f62a91bca6569a619da78
Angel <Angel@DESKTOP-LKGFTP4>
2023-04-21 16:57:42 +0200

    clase campania (angel)

--- src/project_tacion_de_oro/Campanya.java
+++ src/project_tacion_de_oro/Campanya.java
@@ -9,5 +9,41 @@ package project_tacion_de_oro;
  * @author usuario
  */
 public class Campanya {
+    private int anio;
+    private String temporada;
+
+    public Campanya() {
+    }
+
+    public Campanya(int anio, String temporada) {
+        this.anio = anio;
+        this.temporada = temporada;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(String temporada) {
+        this.temporada = temporada;
+    }
+    
+    public static void abrirCampaña(){
+        
+    }
+    public static void cerrarCampaña(){
+        
+    }
+    public static void anadirArticulo(Articulo articulo){
+        
+    }
     
 }
